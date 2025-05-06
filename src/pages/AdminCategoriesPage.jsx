ca26ff121ff217679005f4eed4acce5d24420ab3
Naima9991 <150269222+Naima9991@users.noreply.github.com>
2025-05-06 16:23:11 +0100

    Fixing import with Admin Products Page

--- src/pages/AdminCategoriesPage.jsx
+++ src/pages/AdminCategoriesPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import '../styles/AdminProductspage.css';
+import '../styles/AdminProductsPage.css';
 
 /* 
   AdminCategoriesPage Component:
