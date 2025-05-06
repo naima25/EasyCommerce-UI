4c0c4ae528633fff2f1446b177c55e1cbea344b4
Naima9991 <150269222+Naima9991@users.noreply.github.com>
2025-05-06 14:56:07 +0100

    Entrance point for React Application

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,14 @@
+/*
+ Main.jsx
+
+  - Entry point for the React application.
+  - StrictMode: Enforces additional checks and warnings for potential issues during development.
+  - AppProvider: Wraps the App component with a context provider to share state across the application.
+  - createRoot: Initializes the root of the React app and renders the App component to the DOM.
+  - index.css: Global styles are applied to the application.
+*/
+
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
